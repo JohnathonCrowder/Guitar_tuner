1620001b9bc0a937423daf8f86d4c908e23fe1d1
Johnathon Crowder <139363360+JohnathonCrowder@users.noreply.github.com>
2024-05-14 17:28:21 -0500

    Adds About and Contact page
    adds about anc conatact page

--- app.py
+++ app.py
@@ -104,6 +104,14 @@ def stop_estimation():
         stream = None
     return jsonify({'message': 'Pitch estimation stopped'})
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     # Run the Flask application
     app.run(debug=True)
